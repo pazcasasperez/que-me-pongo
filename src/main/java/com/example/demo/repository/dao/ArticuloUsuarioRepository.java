112752b07c3b91d026424ce714921d6a29a6a026
pazcasasperez <marcasper16@alu.edu.gva.es>
2025-01-15 19:57:49 +0100

    ArticuloUsuario

--- src/main/java/com/example/demo/repository/dao/ArticuloUsuarioRepository.java
+++ src/main/java/com/example/demo/repository/dao/ArticuloUsuarioRepository.java
@@ -1,5 +1,14 @@
 package com.example.demo.repository.dao;
 
-public interface ArticuloUsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.repository.entity.ArticuloUsuario;
+
+import jakarta.transaction.Transactional;
+
+@Repository
+@Transactional
+public interface ArticuloUsuarioRepository  extends JpaRepository<ArticuloUsuario, Long>{
 
 }
