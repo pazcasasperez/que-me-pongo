6a12ca251892b6adbb1f2b8aa64eaf85a5cf80b9
Lumonbel <lmonfortbeltran@gmail.com>
2025-01-15 18:16:56 +0100

    conexion backend con frontend

---
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica CORS a todos los endpoints
+                .allowedOrigins("http://localhost:4200") // Dominio del cliente Angular
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos permitidos
+                .allowedHeaders("*") // Permite todos los encabezados
+                .allowCredentials(true); // Permite cookies/sesiones compartidas si es necesario
+    }
+}
